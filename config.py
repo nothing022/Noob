80fd587ec937d6ae189596aff9f30d61d5d1b6a5
Nᴏᴛʜɪɴɢ... <125535875+nothing022@users.noreply.github.com>
2024-11-13 15:07:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NOOB-CODER-RAJA/ALPHA",
+    "https://github.com/nothing022/Noob",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
